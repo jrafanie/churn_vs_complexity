c1c2cdd89bbadb9d913905540a07c8fc94326982
Joe Rafaniello <jrafanie@redhat.com>
2016-08-05 18:48:11 -0400

    Fix some style with rubocop -a

--- bin/churn_vs_complexity.rb
+++ bin/churn_vs_complexity.rb
@@ -5,22 +5,22 @@ require 'parallel'
 git_repo = ARGV[0]
 
 def ignored?(file_path)
-  return true unless file_path.end_with?(".rb")
-  return true if file_path.start_with?("spec")
-  return true if file_path.start_with?("test")
-  return true if file_path.start_with?("db/migrate")
+  return true unless file_path.end_with?('.rb')
+  return true if file_path.start_with?('spec')
+  return true if file_path.start_with?('test')
+  return true if file_path.start_with?('db/migrate')
 
   # Exclude everything in config except for common rails files/directories
-  if file_path.start_with?("config")
-    return false if file_path == "config/routes.rb"
-    return false if file_path == "config/application.rb"
-    return false if file_path.start_with?("config/initializers")
-    return false if file_path.start_with?("config/environments")
+  if file_path.start_with?('config')
+    return false if file_path == 'config/routes.rb'
+    return false if file_path == 'config/application.rb'
+    return false if file_path.start_with?('config/initializers')
+    return false if file_path.start_with?('config/environments')
     return true
   end
 
-  return true if file_path.end_with?("_spec.rb")
-  return true if file_path.end_with?("_test.rb")
+  return true if file_path.end_with?('_spec.rb')
+  return true if file_path.end_with?('_test.rb')
   return true unless File.file?(file_path)
   false
 end
@@ -49,13 +49,13 @@ Dir.chdir(git_repo) do
   results.sort! { |x, y| x.first <=> y.first }
 end
 
-ui_csv      = CSV.open("./www/churn_vs_complexity_controllers_helpers.csv", "wb")
-backend_csv = CSV.open("./www/churn_vs_complexity_backend.csv", "wb")
-ui_csv << ["file", "churn", "total_complexity", "average_method_complexity"]
-backend_csv << ["file", "churn", "total_complexity", "average_method_complexity"]
+ui_csv      = CSV.open('./www/churn_vs_complexity_controllers_helpers.csv', 'wb')
+backend_csv = CSV.open('./www/churn_vs_complexity_backend.csv', 'wb')
+ui_csv << %w(file churn total_complexity average_method_complexity)
+backend_csv << %w(file churn total_complexity average_method_complexity)
 
 results.each do |r|
-  if /app\/helpers|app\/controllers|app\/presenters/.match(r[0])
+  if /app\/helpers|app\/controllers|app\/presenters/ =~ r[0]
     ui_csv << r
   else
     backend_csv << r
--- config.ru
+++ config.ru
@@ -1,12 +1,10 @@
-www = File.expand_path(File.join(File.dirname(__FILE__), "www"))
+www = File.expand_path(File.join(File.dirname(__FILE__), 'www'))
 
-run Proc.new { |env|
+run proc { |env|
   req = Rack::Request.new(env)
-  index_file = File.join(www, req.path_info, "index.html")
+  index_file = File.join(www, req.path_info, 'index.html')
 
-  if File.exists?(index_file)
-    req.path_info += "index.html"
-  end
+  req.path_info += 'index.html' if File.exist?(index_file)
 
   Rack::Directory.new(www).call(env)
 }
