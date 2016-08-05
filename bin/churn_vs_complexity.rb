bad4851eb0d8ace6c9efb3b54efc72b845fbe6cd
Joe Rafaniello <jrafanie@redhat.com>
2016-08-05 15:58:09 -0400

    Change to the git repo directory provided in ARGV.
    Don't assume we're in the git repo.
    Write the output to ./www/...csv files.

--- bin/churn_vs_complexity.rb
+++ bin/churn_vs_complexity.rb
@@ -2,10 +2,7 @@ require 'yaml'
 require 'csv'
 require 'parallel'
 
-yml = `churn -d "07/15/15" -y`
-changes = YAML.load(yml)[:churn][:changes]
-
-require "csv"
+git_repo = ARGV[0]
 
 def ignored?(file_path)
   return true unless file_path.end_with?(".rb")
@@ -16,25 +13,31 @@ def ignored?(file_path)
   false
 end
 
-# run just the first 20 or so churn files
-# changes = changes[0..20]
+results = nil
+Dir.chdir(git_repo) do
+  yml = `churn -d "07/15/15" -y`
+  changes = YAML.load(yml)[:churn][:changes]
 
-results = Parallel.map(changes, in_processes: 8) do |change|
-  next if ignored?(change[:file_path])
-  puts "file_path: #{change[:file_path]}"
+  # run just the first 20 or so churn files
+  # changes = changes[0..20]
 
-  output = `flog -sq #{change[:file_path]}`
-  flog_total = /([0-9]+\.[0-9]+): flog total/.match(output)[1]
-  flog_method_avg = /([0-9]+\.[0-9]+): flog\/method average/.match(output)[1]
-  puts "file_path: #{change[:file_path]}, changes: #{change[:times_changed]}, flog total: #{flog_total}, flog method avg: #{flog_method_avg}"
-  [change[:file_path], change[:times_changed], flog_total, flog_method_avg]
+  results = Parallel.map(changes, in_processes: 8) do |change|
+    next if ignored?(change[:file_path])
+    puts "file_path: #{change[:file_path]}"
+
+    output = `flog -sq #{change[:file_path]}`
+    flog_total = /([0-9]+\.[0-9]+): flog total/.match(output)[1]
+    flog_method_avg = /([0-9]+\.[0-9]+): flog\/method average/.match(output)[1]
+    puts "file_path: #{change[:file_path]}, changes: #{change[:times_changed]}, flog total: #{flog_total}, flog method avg: #{flog_method_avg}"
+    [change[:file_path], change[:times_changed], flog_total, flog_method_avg]
+  end
+
+  # remove ignored files
+  results.compact!
 end
 
-# remove ignored files
-results.compact!
-
-ui_csv      = CSV.open("churn_vs_complexity_controllers_helpers.csv", "wb")
-backend_csv = CSV.open("churn_vs_complexity_backend.csv", "wb")
+ui_csv      = CSV.open("./www/churn_vs_complexity_controllers_helpers.csv", "wb")
+backend_csv = CSV.open("./www/churn_vs_complexity_backend.csv", "wb")
 ui_csv << ["file", "churn", "total_complexity", "average_method_complexity"]
 backend_csv << ["file", "churn", "total_complexity", "average_method_complexity"]
 
